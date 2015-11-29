ff720a9e75c8feda4605b53a45cbc273068ed1f3
Rián an gealt <rgrier685@gmail.com>
2015-11-28 22:01:23 -0600

    More things
    More things

--- BikeShare/app/src/main/java/edu/iastate/bikeshare/Maintenance.java
+++ BikeShare/app/src/main/java/edu/iastate/bikeshare/Maintenance.java
@@ -2,8 +2,11 @@ package edu.iastate.bikeshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class Maintenance extends AppCompatActivity {
+
+    private Spinner spin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,6 @@ public class Maintenance extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Request Maintenance");
         setContentView(R.layout.activity_maintenance);
+        spin = (Spinner) findViewById(R.id.spinner);
     }
 }
--- BikeShare/app/src/main/java/edu/iastate/bikeshare/Move_Requests.java
+++ BikeShare/app/src/main/java/edu/iastate/bikeshare/Move_Requests.java
@@ -2,8 +2,15 @@ package edu.iastate.bikeshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class Move_Requests extends AppCompatActivity {
+
+    private LinearLayout screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,17 @@ public class Move_Requests extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Move Requests");
         setContentView(R.layout.activity_move__requests);
+        screen = (LinearLayout) findViewById(R.id.linearLayout);
+        setupView();
+    }
+
+    private void setupView(){
+        for(int i = 0; i< 10; i++){
+            Button textview = new Button(this);
+            textview.setText("Request " + i);
+            screen.addView(textview);
+        }
     }
 }
+
+
